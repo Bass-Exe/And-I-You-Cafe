c6b48909b15eb5e1be771175bfe041819b143938
HeroCoding <112276716+Bass-Exe@users.noreply.github.com>
2025-05-15 15:31:04 -0700

    i

--- And I You cafe Site/main.js
+++ And I You cafe Site/main.js
@@ -1,7 +1,7 @@
 let current = 1;
 
 
-const changeSlide = () => {
+const changeSlides = () => {
     const slideList = document.querySelectorAll('.slide');
     const slides = Array.from(slideList);
 
@@ -15,14 +15,14 @@ const changeSlide = () => {
 
 
     slides.forEach(slide => {
-        if(slide.classList[1].split('-')[1] * 1 === current){
-        slide.style.cssTest = "visibility: visible; opacity : 1;"
-    } else{
-        slide.style.cssTest = "visibility: hidden;  opacity : 0;"
+        if(slide.classList[1].split('-')[1] * 1 === current) {
+        slide.style.cssText = "visibility: visible; opacity : 1;";
+    } else {
+        slide.style.cssText = "visibility: hidden;  opacity : 0;";
      }
     
     });
 }
 
-changeSlide();
+changeSlides();
 
