ea154aac4960a65917843ced9bb06ae95231f2ba
HeroCoding <112276716+Bass-Exe@users.noreply.github.com>
2025-05-22 20:02:10 -0700

    update 2.0

--- And I You cafe Site/main.js
+++ And I You cafe Site/main.js
@@ -1,13 +1,12 @@
-const carouselContainer = document.querySelector('.carousel-container');
-const carouselItems = document.querySelectorAll('.slides');
-
+const carouselContainer = document.querySelector(".carousel-container");
+const carouselItems = document.querySelectorAll(".slides");
 
 let currentIndex = 1;
 let intervalId;
 
 function showSlide(index) {
-  carouselItems.forEach((item) => item.classList.remove('active'));
-  carouselItems[index].classList.add('active');
+  carouselItems.forEach((item) => item.classList.remove("active"));
+  carouselItems[index].classList.add("active");
 }
 
 function nextSlide() {
@@ -16,35 +15,37 @@ function nextSlide() {
 }
 
 function prevSlide() {
-  currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+  currentIndex =
+    (currentIndex - 1 + carouselItems.length) % carouselItems.length;
   showSlide(currentIndex);
 }
 
 function startAutoplay() {
-    intervalId = setInterval(nextSlide, 3000); // Change slide every 3 seconds
+  intervalId = setInterval(nextSlide, 3000); // Change slide every 3 seconds
 }
 
 function stopAutoplay() {
-    clearInterval(intervalId);
+  clearInterval(intervalId);
 }
 
 // Initialize the carousel and start autoplay
 showSlide(currentIndex);
 startAutoplay();
 
-
 function handleIntersection(entries) {
-  entries.forEach(entry => {
+  entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('is-visible'); // Add the class when in view
+      entry.target.classList.add("is-visible"); // Add the class when in view
       observer.unobserve(entry.target); // Stop observing once triggered
     }
   });
 }
 
-const observer = new IntersectionObserver(handleIntersection, { threshold: 0.4});//threshold is used to see how far till it in the viewer sight lower the faster
+const observer = new IntersectionObserver(handleIntersection, {
+  threshold: 0.4,
+}); //threshold is used to see how far till it in the viewer sight lower the faster
 
-const fadeInSectionElements = document.querySelectorAll('.hidden');
-fadeInSectionElements.forEach(element => {
+const fadeInSectionElements = document.querySelectorAll(".hidden");
+fadeInSectionElements.forEach((element) => {
   observer.observe(element); // Start observing each element
 });
